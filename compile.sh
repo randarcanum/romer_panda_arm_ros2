6e501284fe493e30cee784182fed68c03f39eb3d
ege <egeecevit@gmail.com>
2025-08-14 18:31:33 +0300

    Comment out SLICER_DIR

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 source /opt/ros/humble/setup.sh
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DFranka_DIR:PATH='/home/mrslabpanda/libfranka/build' -DSlicer_DIR:PATH='/home/mrslabpanda/Slicer-SuperBuild-Debug/Slicer-build' -Wno-dev
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DFranka_DIR:PATH='/home/mrslabpanda/libfranka/build' # -DSlicer_DIR:PATH='/home/mrslabpanda/Slicer-SuperBuild-Debug/Slicer-build' -Wno-dev
 
